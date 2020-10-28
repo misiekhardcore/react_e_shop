faee7bc86685e4046a8c3a44bb50a2efc20c72ca
misiekhardcore <misiekhardcore@gmail.com>
2020-10-28 11:49:38 +0100

    categories filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -47,7 +47,16 @@ const App = (props) => {
           )}
         />
         <Route
+          exact
           path="/search"
+          render={() => (
+            <MainLayout>
+              <Search />
+            </MainLayout>
+          )}
+        />
+        <Route
+          path="/search/:filterType"
           render={() => (
             <MainLayout>
               <Search />
--- my-app/src/components/ProductResults/index.js
+++ my-app/src/components/ProductResults/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
 import { fetchProductsStart } from "../../redux/Product/product.actions";
+import Select from "./../forms/Select";
 import "./styles.scss";
 
 import Product from "./Product";
@@ -11,10 +13,17 @@ const mapState = ({ productsData }) => ({
 
 const ProductResults = ({}) => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { filterType } = useParams();
   const { products } = useSelector(mapState);
   useEffect(() => {
-    dispatch(fetchProductsStart());
-  }, []);
+    dispatch(fetchProductsStart({ filterType }));
+  }, [filterType]);
+
+  const handleSelect = (e) => {
+    const nextFilter = e.target.value;
+    history.push(`/search/${nextFilter}`);
+  };
 
   if (!Array.isArray(products)) {
     return null;
@@ -26,10 +35,35 @@ const ProductResults = ({}) => {
       </div>
     );
   }
+
+  const configSelect = {
+    defaultValue: filterType,
+    label: "Order by",
+    options: [
+      {
+        name: "Show all",
+        value: "",
+      },
+      {
+        name: "Mens",
+        value: "mens",
+      },
+      {
+        name: "Womens",
+        value: "womens",
+      },
+    ],
+    handleChange: handleSelect,
+  };
+
   return (
     <div className="products">
+      <h1>Product Browser</h1>
+
+      <Select {...configSelect} />
+
       {products.map((product, index) => {
-        return <Product {...product} />;
+        return <Product key={index} {...product} />;
       })}
     </div>
   );
